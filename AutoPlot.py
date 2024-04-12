bf9043d003d23a106ba44566f28c7ede42ae9b8f
Adam Mason <60157744+admason@users.noreply.github.com>
2024-04-12 16:46:45 +0100

    Update AutoPlot.py

--- AutoPlot.py
+++ AutoPlot.py
@@ -21,13 +21,16 @@ class Plot:
         x_values = (range(self.x_lower, self.x_upper + 1))
         y_values = list(map(lambda x: eval(fx), x_values))
        
-        return x_values,        y_values,        plt.style.use('seaborn'),        plt.plot(x_values, y_values),        #plt.axis([min(x_values),max(x_values)*1.1,min(y_values),max(y_values)*1.1]),\
-        plt.axis([self.x_lower,self.x_upper,min(y_values),max(y_values)*1.1]),        plt.show()
+        #return x_values,\
+        #y_values,\
+        return plt.style.use('seaborn'),\
+        plt.plot(x_values, y_values),\
+        plt.axis([min(x_values),max(x_values)*1.1,min(y_values),max(y_values)*1.1]),\
+        
+        plt.show()
 
-    plot_1 = Plot(fx,x_lower, x_upper) 
+plot_1 = Plot(fx,x_lower, x_upper) 
 plot_1.plotter()
-
-
 # In[ ]:
 
 
